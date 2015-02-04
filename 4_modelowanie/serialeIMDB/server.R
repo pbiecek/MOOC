b194eb865c5a38fac2b358f5f7759b0b52457a93
pbiecek <przemyslaw.biecek@gmail.com>
2015-02-04 23:08:24 +0100

    upload na shiny apps

--- 4_modelowanie/serialeIMDB/server.R
+++ 4_modelowanie/serialeIMDB/server.R
@@ -22,20 +22,22 @@ shinyServer(function(input, output) {
   
   mySerial %>%
     ggvis(x = ~id, y = ~ocena, fill = ~sezon) %>%
+    # ewentualnie group_by(sezon)
     layer_text(text := ~nazwa, opacity=0, fontSize:=1) %>%
     layer_points(fillOpacity:=0.8) %>%
     hide_legend("fill") %>%
     hide_axis("x") %>%
-    set_options(width = 800,padding = padding(10, 10, 50, 50)) %>%
-#    scale_numeric("y", domain=c(0, 10)) %>%
+    set_options(width = 640,padding = padding(10, 10, 50, 50)) %>%
+    #    scale_numeric("y", domain=c(0, 10)) %>%
     add_axis("x", title = "Numer odcinka", 
              properties = axis_props(
-      grid = list(stroke = "white")      
-    )) %>% layer_model_predictions(model = "lm") %>%
+               grid = list(stroke = "white")      
+             )) %>% 
+    layer_model_predictions(model = "lm") %>%
     add_tooltip(function(data){
       paste0(data$nazwa, "<br>ocena: ",as.character(data$ocena))
     }, "hover") %>%
     bind_shiny("serialPlot")
-
+  
 })
 
