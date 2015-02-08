d71c41fc9ed0ebdf636fd9b0c4e969ab6b99a8e6
pbiecek <przemyslaw.biecek@gmail.com>
2015-02-08 13:47:29 +0100

    google analytics

--- 4_modelowanie/serialeIMDB/ui.R
+++ 4_modelowanie/serialeIMDB/ui.R
@@ -54,7 +54,13 @@ seriale <- c("'Allo 'Allo!", "24", "Adventure Time", "Agatha Christie's Poirot",
              "X-Men", "Young Justice")
 
 shinyUI(fluidPage(
-  tags$head(tags$style(HTML("
+  tags$head(tags$script("(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+                  ga('create', 'UA-5650686-6', 'icm.edu.pl');
+                  ga('send', 'pageview');"),
+                tags$style(HTML("
                             .ggvis-output-container {
                             margin-left: auto ;
                             margin-right: auto ;
