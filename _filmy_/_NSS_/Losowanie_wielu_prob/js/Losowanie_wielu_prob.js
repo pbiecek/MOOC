a89da830197672dc730144d5fbbbd4fadc6551f9
pbiecek <przemyslaw.biecek@gmail.com>
2015-03-23 20:11:53 +0100

    NSS

---
+++ _filmy_/_NSS_/Losowanie_wielu_prob/js/Losowanie_wielu_prob.js
@@ -0,0 +1,12 @@
+(function($) {
+    $(document).ready(function() {
+	
+	$('#Losowanie_wielu_prob').scianimator({
+	    'images': ['images/Losowanie_wielu_prob1.png', 'images/Losowanie_wielu_prob2.png', 'images/Losowanie_wielu_prob3.png', 'images/Losowanie_wielu_prob4.png', 'images/Losowanie_wielu_prob5.png', 'images/Losowanie_wielu_prob6.png', 'images/Losowanie_wielu_prob7.png', 'images/Losowanie_wielu_prob8.png', 'images/Losowanie_wielu_prob9.png', 'images/Losowanie_wielu_prob10.png', 'images/Losowanie_wielu_prob11.png', 'images/Losowanie_wielu_prob12.png', 'images/Losowanie_wielu_prob13.png', 'images/Losowanie_wielu_prob14.png', 'images/Losowanie_wielu_prob15.png', 'images/Losowanie_wielu_prob16.png', 'images/Losowanie_wielu_prob17.png', 'images/Losowanie_wielu_prob18.png'],
+	    'width': 480,
+	    'delay': 50,
+	    'loopMode': 'loop'
+	});
+	$('#Losowanie_wielu_prob').scianimator('play');
+    });
+})(jQuery);
